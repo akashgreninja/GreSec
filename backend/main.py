20a0fdf712425e16208009202b4e832ff3123467
Akashgreninja <akashuhulekal@gmail.com>
2023-10-23 23:39:05 +0530

    builtthe starter template

---
+++ backend/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=4001)
